ca26af9ac6a5f753ac876ed1ebb5e5a6b1de82d6
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-03-18 16:10:44 +0100

    first routing

--- book-rating/src/app/app-routing.module.ts
+++ book-rating/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'book/:isbn', component: BookDetailsComponent },
+  // { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
