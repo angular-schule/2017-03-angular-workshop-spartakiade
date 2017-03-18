5fe5d23442675d1c4c3b24fea8088ace5b2f147a
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-03-18 17:07:49 +0100

    fun with RxJS

--- book-rating/src/app/book-details/book-details.component.ts
+++ book-rating/src/app/book-details/book-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-book-details',
@@ -6,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-details.component.css']
 })
 export class BookDetailsComponent implements OnInit {
-
-  constructor() { }
+  isbn: string;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+      .map(params => params['isbn'] as string)
+      // .filter(isbn => isbn === '222')
+      .subscribe((isbn) => this.isbn = isbn);
   }
-
 }
